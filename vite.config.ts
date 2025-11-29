80c711b0a05f29966b89a2285820b0a5c165965f
Ayush Verma <ayush2003sitapur@gmail.com>
2025-11-29 18:10:54 +0530

    Seo

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://ayush-2003.netlify.app',
+    })
+  ],
   server: {
     port: 3000,
     open: true,
